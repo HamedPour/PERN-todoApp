5e8e1ad5a85fdb35a6fff4fe7614dce34ca44d58
HamedPour <hamed_k.pour@hotmail.com>
2020-07-22 16:28:48 +0100

    React init

---
+++ client/src/views/Homepage/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// styled components
+import Container from "./styledComponents/Container";
+
+function HomePage() {
+  return <Container>HOMEPAGE</Container>;
+}
+
+export default HomePage;
